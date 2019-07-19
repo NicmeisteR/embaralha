fa8a8599fefbf4657405995520942681cd824b8e
Nicolaas Nel <nicon@infoslips.com>
2019-07-19 13:20:02 +0200

    Issue #2 Fix, Multiple Hover Wrong Text Issue

--- example/example.js
+++ example/example.js
@@ -9,8 +9,16 @@ const throttledEmbaralha = (elem, time) => {
 const main = () => {
   const body = document.querySelector('body');
   for (const elem of body.childNodes) {
-    elem.addEventListener('mouseover', throttledEmbaralha(elem, 1000));
+    // Added a timeout to stop it from executing multiple times at once,
+    // Note the timeout is 50 more than initial because it might go over when both are 1000
+    setTimeout(() => {elem.addEventListener('mouseover', throttledEmbaralha(elem, 1000))}, 1050);
   }
+
+  // If you want to target classes instead of every element on the page.
+  // const text = document.getElementsByClassName('text');
+  // for (const elem of text) {
+  //   setTimeout(() => {elem.addEventListener('mouseover', throttledEmbaralha(elem, 1000))}, 1050);
+  // }
 }
 
 main();
